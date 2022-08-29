5f1d41661d52d6b4a2441daa4db7975a9272a5a4
Patrick Barth <patrick.barth@computational.bio.uni-giessen.de>
2022-08-29 13:58:05 +0200

    add merging script for wig files

---
+++ bin/merge-wig.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import argparse
+import os
+from wig_files_writer import *
+
+
+
+
+#####################################################
+# Merges several replicates (as wig files) into a	#
+#  representative version							#
+#####################################################
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--wig', '-w', nargs='+', help='Wig files to be merged into a single representative file')
+parser.add_argument('--output', '-o', help='Name of the output file')
+parser.add_argument('what_shall_i_write_here', nargs=argparse.REMAINDER)
+args = parser.parse_args()
+
+#######################
+#######################
+###    variables    ###
+#######################
+#######################
+
+
+########################
+########################
+###    arguments     ###
+########################
+########################
+
+replicates = args.wig
+
+##########################################################################################################################################
+
+###################
+###################
+### main script ###
+###################
+###################
+
+def main():
+	print("\nStarting merging wig files")
+	print( str(  len(replicates) ) + " replicates used for merging\n" )
+
+	parsedReplicates = []
+	
+
+	# Parses all wig files used as input. Per cycle one replicate consisting of a forward and a reverse file will be parsed.
+	# Additionally, the total amount of hits per replicate is calculated
+
+	for replicate in replicates:
+		parsedReplicates.append(parse_wig2(replicate))
+
+	mergedWig = merge_wig(parsedReplicates)
+
+	write_wig2(mergedWig, args.output)
+	
+	print("\nmerging complete\n")
+
+#######################
+#######################
+###    functions    ###
+#######################
+#######################
+
+
+#####################################
+# Merges all given wig files.		#
+# Returns a representative wig file #
+#####################################
+
+def merge_wig(wigFiles):
+	# get the names of all chromosomes
+	chromosomeNames = []
+	for k in wigFiles:
+		chromosomeNames += k.keys()
+		#chromosomeNames = wigFiles[0].keys()
+	chromosomeNames = list(set(chromosomeNames))
+	mergedChromosomes = {}
+	numberReplicates = len(wigFiles)
+	#go through every chromosome and get a unique list off all positions of all involved data sets. 
+	for chromosome in chromosomeNames:
+		mergedChromosomes[chromosome] = {}
+		allPositions = []
+		for wig in wigFiles:
+			if chromosome in wig.keys():
+				allPositions += wig[chromosome].keys()
+		allPositions = list(set(allPositions))
+		# create the mean hitCount for every position
+		for position in allPositions:
+			addedCountsForward = 0
+			addedCountsReverse = 0
+			for wig in wigFiles:
+				if chromosome in wig.keys():
+					if position in wig[chromosome].keys():
+						addedCountsForward += wig[chromosome][position]["forward"]
+						addedCountsReverse += wig[chromosome][position]["reverse"]
+			mergedChromosomes[chromosome][position] = {}
+			mergedChromosomes[chromosome][position]["forward"] = addedCountsForward / numberReplicates
+			mergedChromosomes[chromosome][position]["reverse"] = addedCountsReverse / numberReplicates
+	return mergedChromosomes
+
+
+
+
+
+#######################
+#######################
+###      tests      ###
+#######################
+#######################
+
+
+
+
+##########################
+### starts main script ###
+##########################
+main()
