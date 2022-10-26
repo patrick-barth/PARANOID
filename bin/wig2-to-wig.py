520d4d39b86b8f5e5266807a3347e08e0ecfc907
Patrick Barth <patrick.barth@computational.bio.uni-giessen.de>
2022-10-26 10:32:04 +0200

    only write wig files when there are asociated cross-link-sites

--- bin/wig2-to-wig.py
+++ bin/wig2-to-wig.py
@@ -50,17 +50,23 @@ def main():
 	reversePeaks = {}
 
 	for chromosome in wig:
-		forwardPeaks[chromosome] = {}
-		reversePeaks[chromosome] = {}
 		for position in wig[chromosome]:
 			if not float(wig[chromosome][position]["forward"]) == 0:
+				if not chromosome in forwardPeaks.keys():
+					forwardPeaks[chromosome] = {}
 				forwardPeaks[chromosome][position] = wig[chromosome][position]["forward"]
 			if not float(wig[chromosome][position]["reverse"]) == 0:
+				if not chromosome in reversePeaks.keys():
+					reversePeaks[chromosome] = {}
 				reversePeaks[chromosome][position] = wig[chromosome][position]["reverse"]  
 
 
-	write_wig(forwardPeaks, outputPrefix + "_forward.wig")
-	write_wig(reversePeaks, outputPrefix + "_reverse.wig")
+	if not len(forwardPeaks) == 0:
+		write_wig(forwardPeaks, outputPrefix + "_forward.wig")
+	if not len(reversePeaks) == 0:
+		write_wig(reversePeaks, outputPrefix + "_reverse.wig")
+
+	
 
 
 #######################
