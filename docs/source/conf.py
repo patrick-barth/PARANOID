99a14dfcedbc703439baf208c05d888def0db381
Patrick Barth <patrick.barth@computational.bio.uni-giessen.de>
2022-11-01 17:45:51 +0100

    Squashed commit of the following:
    commit dc951c0e3912562f945a0f53239454cabb919dda
    Author: Patrick Barth <patrick.barth@computational.bio.uni-giessen.de>
    Date:   Tue Nov 1 17:24:00 2022 +0100
    
        test
    
    commit c491d255d6f85ed05e06cecaa503812e011400ab
    Author: Patrick Barth <patrick.barth@computational.bio.uni-giessen.de>
    Date:   Tue Nov 1 17:22:40 2022 +0100
    
        test
    
    commit dd5ea13dbbbc7eaa40c7d0cb703a3ef0a4a5d595
    Author: Patrick Barth <patrick.barth@computational.bio.uni-giessen.de>
    Date:   Tue Nov 1 17:18:05 2022 +0100
    
        add python dependency
    
    commit 0b6ed662bc6ea67fdd28042493580bebc37bdf71
    Author: Patrick Barth <patrick.barth@computational.bio.uni-giessen.de>
    Date:   Tue Nov 1 17:06:28 2022 +0100
    
        test
    
    commit c824a9024d31e5fe06c3e50d36cf818087bb6329
    Author: Patrick Barth <patrick.barth@computational.bio.uni-giessen.de>
    Date:   Tue Nov 1 16:42:00 2022 +0100
    
        change documentation theme to rtd theme
    
    commit 2f98a2ded6efb7ce10e8ba4eea785b028630c6a2
    Author: Patrick Barth <patrick.barth@computational.bio.uni-giessen.de>
    Date:   Tue Nov 1 16:27:45 2022 +0100
    
        add pages to all menu points
    
    commit b542d9d3cc1c0f4861a2da5b41a001b262a51ce7
    Author: Patrick Barth <patrick.barth@computational.bio.uni-giessen.de>
    Date:   Tue Nov 1 16:08:54 2022 +0100
    
        build with sphinx
    
    commit b1821cfc816ef491285937f539bea630b598babe
    Author: Patrick Barth <patrick.barth@computational.bio.uni-giessen.de>
    Date:   Tue Nov 1 15:59:39 2022 +0100
    
        test tree for rtd; different file
    
    commit 74cd381af93bc1c6b74e891cdd71aca3f19e30bc
    Author: Patrick Barth <patrick.barth@computational.bio.uni-giessen.de>
    Date:   Tue Nov 1 15:53:25 2022 +0100
    
        test tree for rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,8 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sphinx_rtd_theme
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -13,7 +15,9 @@ author = 'Patrick Barth'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_rtd_theme'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +28,5 @@ master_doc = 'index'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
