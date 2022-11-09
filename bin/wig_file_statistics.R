e28d42bc04f4cd27709aeef86445bb2dfeee9213
Patrick Barth <patrick.barth@computational.bio.uni-giessen.de>
2022-11-09 17:40:23 +0100

    adapt wig file statistics to be run in a pipeline

--- bin/wig_file_statistics.R
+++ bin/wig_file_statistics.R
@@ -4,71 +4,47 @@ library(optparse)
 library(ggplot2)
 library(wig)
 
-filelist_fw <- c(
-  "RVFV_virions/raw-wig-files/A_rep_1_forward.wig",
-  "RVFV_virions/raw-wig-files/A_rep_2_forward.wig",
-  "RVFV_virions/raw-wig-files/A_rep_3_forward.wig"
-)
-
-filelist_rev <- c(
-  "RVFV_virions/raw-wig-files/A_rep_1_reverse.wig",
-  "RVFV_virions/raw-wig-files/A_rep_2_reverse.wig",
-  "RVFV_virions/raw-wig-files/A_rep_3_reverse.wig"
-)
-
-
 #### Get arguments ##
-option_list = list(
-  make_option(c("-l","--logs"), type="character",default=NULL,
-              help="Log file containing information about barcode splitting", metavar="character"),
-  make_option(c("-o","--output"), type="character",default="barcode_distribution",
-              help="Name of output plot", metavar="character"),
-  make_option(c("-t","--type"), type="character",default="png",
-              help="File type of output plot", metavar="character"),
-  make_option(c("-c", "--color"), type="character", default="#69b3a2", 
-              help="Color", metavar="character")
-);
-
-library(wig)
-
-chromosome_lengths <- list(
-  "",
-  "",
-  ""
+option_list <- list(
+  make_option(c("-i", "--input_path"),    type = "character",   default = ".",
+              metavar = "character",      help = "Working directory containing wig files to compare"),
+  make_option(c("-l", "--chrom_length"),  type = "character",   default = ".",
+              metavar = "character",      help = "File containing lengths of chromosomes"),
+  make_option(c("-o", "--output"),        type = "character",   default = "output",
+              metavar = "character",      help = "Output base name"),
+  make_option(c("-t", "--type"),          type = "character",   default = "png",
+              metavar = "character",      help = "File type of output plot")
 )
+opt_parser  <- OptionParser(option_list=option_list)
+opt         <- parse_args(opt_parser)
+
+input_path    <- opt$input_path
+chrom_length  <- opt$chrom_length
+output        <- opt$output
+type          <- opt$type
+
+chrom_length <- read.csv(chrom_length, header = FALSE, sep = "\t", row.names = 1)
+# This line is only an adaption to the import_wig function which cuts chrom names at the first encountered dot
+row.names(chrom_length) <- unlist(lapply(strsplit(row.names(chrom_length),"\\."), `[`, 1))
+files <- list.files(path = input_path, pattern = "\\.wig$")
 
 import_wig_files<-function(files=vector())
 {
-  chromosomes = vector();
-  chromosome_length = list();
+  chromosomes = row.names(chrom_length)
   chromosome_values = list();
   chr_dat = list();
   filelist = list();
   
   for(i in seq(1,length(files)))
   {
-    filelist[[i]] <- import_wig(files[i])
-    # generate a list of chromosome names
-    chromosomes=levels(factor(c(chromosomes, levels(as.factor(filelist[[i]]$chr)))))
-  }
-  
-  # estimate the length of the chromosomes
-  for(chr in chromosomes)
-  {
-    chromosome_length[[chr]] = 0
-    for (i in seq(1,length(files)))
-    {
-      chromosome_length[[chr]] = max(
-        chromosome_length[[chr]],
-        as.vector(filelist[[i]][filelist[[i]]$chr==chr,]$pos)
-      );
-    }
+    full_path <- paste(input_path, "/", files[i], sep = "")
+    filelist[[i]] <- import_wig(full_path)
   }
   
   # generate a list of chromosome matrices with all values
   for(chr in chromosomes)
   {
-    chromosome_values[[chr]] = matrix(nrow=chromosome_length[[chr]], ncol=length(files),data=c(0))
+    chromosome_values[[chr]] = matrix(nrow=chrom_length[chr,], ncol=length(files),data=c(0))
     # loop through our input files and import the values into our matrix
     for (i in seq(1,length(files)))
     {
@@ -82,7 +58,7 @@ import_wig_files<-function(files=vector())
   # build the result list
   for(chr in chromosomes)
   {
-    chr_dat[[chr]] = list(name=chr, len=chromosome_length[[chr]], val=chromosome_values[[chr]])
+    chr_dat[[chr]] = list(name=chr, len=chrom_length[chr,], val=chromosome_values[[chr]])
   }
   
   return(chr_dat);
@@ -90,17 +66,22 @@ import_wig_files<-function(files=vector())
 
 do_cor_analysis<-function(files=vector(), dat=list(), output_filename_dir=paste(getwd(),"/",sep=""))
 {
-  for(chr in seq(1, length(dat)))
+  appended_values <- data.frame(dat[[1]]$val)
+  for(chr in seq(2, length(dat)))
   {
-    chr_name <- dat[[chr]]$name
-    chr_len <- dat[[chr]]$len
-    cat("Chromosome: ", chr_name, "; length ", chr_len, " bp\n");
+    tmp_df <- data.frame(dat[[chr]]$val)
+    appended_values <- rbind(appended_values, tmp_df)
+  }
+    
+    #chr_name <- dat[[chr]]$name
+    #chr_len <- dat[[chr]]$len
+    #cat("Chromosome: ", chr_name, "; length ", chr_len, " bp\n");
     
     cor_res <- cor(dat[[chr]]$val)
     colnames(cor_res)<-basename(files)
     rownames(cor_res)<-colnames(cor_res)
     
-    csv_output_file<-paste(output_filename_dir, chr_name, "_", chr_len, "bp.csv", sep="")
+    csv_output_file<-paste(output,"_correlation.csv", sep="")
     cat("Writing tabular output to ", csv_output_file, "...\n");
     write.csv(cor_res, file=csv_output_file)
     
@@ -108,12 +89,11 @@ do_cor_analysis<-function(files=vector(), dat=list(), output_filename_dir=paste(
     
     # print all the stuff
     heatmap(cor_res,symm=TRUE)
-    heatmap_output_file<-paste(output_filename_dir, chr_name, "_", chr_len, "bp.pdf", sep="")
+    heatmap_output_file<-paste(output,"_correlation.", type, sep="")
     pdf(file=heatmap_output_file, paper="a4")
     heatmap(cor_res,symm=TRUE)
     dev.off()
-  }
+  
 }
 
-do_cor_analysis(filelist_fw, import_wig_files(filelist_fw), output_filename_dir = paste(getwd(), "/forward_", sep=""))
-do_cor_analysis(filelist_rev, import_wig_files(filelist_rev), output_filename_dir = paste(getwd(), "/reverse_", sep=""))
+do_cor_analysis(files, import_wig_files(files), output_filename_dir = paste(input_path, "/forward_", sep=""))
