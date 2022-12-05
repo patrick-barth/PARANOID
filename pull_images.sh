db3b54797c7b2aecd433ff72e10bddeb446f315a
Patrick Barth <patrick.barth@computational.bio.uni-giessen.de>
2022-12-05 18:00:26 +0100

    add script to pull images. Should be run before pipeline to avoid errors

---
+++ pull_images.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+ACCOUNT="pbarth"
+VERSION="1.0"
+REPO="hub.docker.com"
+
+DOCKERDIR=$1
+OUTPUTDIR=$2
+
+set -e
+
+echo "Directory containing dockerfiles: $DOCKERDIR"
+echo "Directory to save images to:      $OUTPUTDIR"
+
+for d in `ls $DOCKERDIR/`; do
+    NAME=${d/dockerfile.}
+    echo "Pulling image $NAME"
+    singularity pull --name $ACCOUNT-$NAME-$VERSION.img docker://$ACCOUNT/$NAME:$VERSION > /dev/null
+done
